7231b1ef36843713a346de28b951440afea5c7b8
tharusha1004 <tharusha.astro@gmail.com>
2024-08-12 00:15:05 +0530

    to successfully connect the db set strict query as false

--- services/schedule-service/config/db.js
+++ services/schedule-service/config/db.js
@@ -3,15 +3,17 @@ const logger = require('../logs/schedule.log');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.set('strictQuery', false); // or true based on your needs
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         logger.info('MongoDB connected');
-    } catch (err) {
-        logger.error(`MongoDB connection error: ${err.message}`);
-        process.exit(1);
+    } catch (error) {
+        logger.error(`MongoDB connection error: ${error.message}`);
+        process.exit(1); // Exit the process with failure
     }
 };
 
 module.exports = connectDB;
+
