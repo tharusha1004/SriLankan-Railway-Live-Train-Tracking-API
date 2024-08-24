e6b7433df1309f54e7ea7e88aece71843d19e6d5
tharusha1004 <tharusha.astro@gmail.com>
2024-08-25 00:39:17 +0530

    implement the basic authentication for the engine management service

---
+++ services/engine-management-service/middleware/basicAuth.js
@@ -0,0 +1,13 @@
+const basicAuth = require('express-basic-auth');
+
+const users = {
+    'admin': 'admin1234',
+    'user': 'user1234'
+};
+
+const authMiddleware = basicAuth({
+    users,
+    challenge: true, 
+});
+
+module.exports = authMiddleware;
