79cd8f14e0d218625a00b473225bce9cdee70de8
tharusha1004 <tharusha.astro@gmail.com>
2024-08-30 01:51:23 +0530

    set the new dependencies in the schdule service and change port into 3003

--- schedule-service/server.js
+++ schedule-service/server.js
@@ -3,17 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./src/utils/logger');
 const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const swaggerDocument = require('./src/swagger/swagger.json');
 const scheduleRoutes = require('./src/routes/scheduleRoutes');
 const mongoose = require('./src/config/db');
 
 const server = express();
 server.use(express.json());
+server.use(cors()); 
+server.use(bodyParser.json());
 
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use('/api', scheduleRoutes);
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 server.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
 });
