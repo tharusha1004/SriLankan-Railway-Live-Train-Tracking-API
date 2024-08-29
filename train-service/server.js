a8c44f6dc71d71b8be38c508297080083e9fbf32
tharusha1004 <tharusha.astro@gmail.com>
2024-08-30 02:06:29 +0530

    change port number into 3004 and add new dependencies into server file in train service

--- train-service/server.js
+++ train-service/server.js
@@ -3,19 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./src/utils/logger');
 const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const swaggerDocument = require('./src/swagger/swagger.json');
 const trainRoutes = require('./src/routes/trainRoutes');
 const mongoose = require('./src/config/db');
 
-const app = express();
-app.use(express.json());
+const server = express();
+server.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api', trainRoutes);
+server.use(cors()); 
+server.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+server.use('/api', trainRoutes);
+
+const PORT = process.env.PORT || 3004;
+server.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
 });
 
-module.exports = app;
+module.exports = server;
