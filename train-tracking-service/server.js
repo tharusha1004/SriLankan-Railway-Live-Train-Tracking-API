6a4e23f617f820ce183b1a3083fd6b384639b825
tharusha1004 <tharusha.astro@gmail.com>
2024-08-30 02:11:21 +0530

    change port number into 3005 and add new dependencies into server file of train tracking service

--- train-tracking-service/server.js
+++ train-tracking-service/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('./src/utils/logger');
 const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const swaggerDocument = require('./src/swagger/swagger.json');
 const trainTrackingRoutes = require('./src/routes/trainTrackingRoutes');
 const mongoose = require('./src/config/db');
@@ -10,10 +12,13 @@ const mongoose = require('./src/config/db');
 const server = express();
 server.use(express.json());
 
+server.use(cors()); 
+server.use(bodyParser.json());
+
 server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 server.use('/api', trainTrackingRoutes);
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3005;
 server.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
 });
