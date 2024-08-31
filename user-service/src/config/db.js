86ff26cc459192c9e4140b724f66f73bf021c5e5
tharusha1004 <tharusha.astro@gmail.com>
2024-08-31 21:01:37 +0530

    deploy to aws ec2

--- user-service/src/config/db.js
+++ user-service/src/config/db.js
@@ -6,7 +6,6 @@ mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
 });
 
-const connectionString = `mongodb+srv://tharusha:tharusha123@slr-train-api.qpazb.mongodb.net/?retryWrites=true&w=majority&appName=SLR-Train-API`;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
